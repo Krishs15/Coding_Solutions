9da6cd7db512f90e30192b20a2f676e92b0794c0
Krishs15 <krishmill19@gmail.com>
2026-09-18 10:42:28 +0530

    ✅ Longest Common Prefix | Easy | Java | Runtime: 1 ms (59.95%) | Memory: 43.2 MB | 2026-09-18

---
+++ leetcode/easy/0014-longest-common-prefix/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0)
+        return "";
+        for(int i=0;i<strs[0].length();i++)
+        {
+            char c=strs[0].charAt(i);
+            for(int k=1;k<strs.length;k++)
+            {
+                if(i==strs[k].length()||strs[k].charAt(i)!=c)
+                {
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
