b3966298482b02c5cab4521f53212557fa61b5d7
Krishs15 <krishmill19@gmail.com>
2026-09-18 09:54:16 +0530

    ✅ Transform String | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ gfg/medium/transform-string5648/solution.java
@@ -0,0 +1,69 @@
+// class Solution {
+//     int transform(String s1, String s2) {
+//         // code here
+        
+//         int l1=s1.length(),l2=s2.length();
+//         if(l1==0||l2==0||l1!=l2)
+//         return -1;
+        
+//         int i,p=0;
+//         for(i=0;i<l2;i++)
+//         {
+//             if(s2.charAt(i)==s1.charAt(0))
+//             {
+//                 p=i+1;
+//                 break;
+//             }
+//         }
+//         String sub=s2.substring(0,p-1);
+//         int sl=sub.length(),k=0;
+        
+//         for(i=0;i<l1;i++)
+//         {
+//             if(s1.charAt(i)==sub.charAt(sl-1))
+//             return k;
+//         }
+        
+//         sub=s1.substring(k-sl,sl);
+//         String sb=sub+s1.substring(0,k-sl)+s1.substring(sl);
+        
+//         if(sb.equals(s2))
+//         return sub.length()+1;
+        
+//         return -1;
+//     }
+// }
+class Solution {
+    int transform (String s1, String s2) {
+        int l1=s1.length(), l2=s2.length();
+
+        if (l1!=l2)
+        return -1;
+        
+        int[] count = new int[128];
+        for (int i=0;i<l1;i++) 
+        {
+            count[s1.charAt(i)]++;
+            count[s2.charAt(i)]--;
+        }
+        for (int i=0;i<128;i++)
+        {
+            if (count[i] != 0) 
+            return -1;
+        }
+        
+        int res=0,i=l1-1,j=l2-1;
+        while(i>=0)
+        {
+            if(s1.charAt(i)==s2.charAt(j))
+            {
+                i--;j--;
+            }
+            else
+            {
+                res++;i--;
+            }
+        }
+        return res;
+    }
+}
