0648214508d972a288977eb7c2ed3c8bd5c25c39
Krishs15 <krishmill19@gmail.com>
2026-09-22 16:38:52 +0530

    ✅ First and Last in Sorted | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-22

--- gfg/medium/first-and-last-occurrences-of-x3116/solution.java
+++ gfg/medium/first-and-last-occurrences-of-x3116/solution.java
@@ -1,16 +1,17 @@
 class Solution {
     ArrayList<Integer> find(int arr[], int x) {
         ArrayList<Integer> res = new ArrayList<>();
-        int l=0, h=arr.length-1, mid=0, p=-1;
+        int first=-1,last=-1;
+        int l=0, h=arr.length-1, mid=0;
         while(l<=h) 
         {
             mid=l+(h-l)/2;
             if (arr[mid]==x) 
             {
-                p=mid;
+                first=mid;
                 h=mid-1; 
             } 
-            else if (arr[mid]>x) 
+            else if(arr[mid]>x) 
             {
                 h=mid-1;
             } 
@@ -19,18 +20,18 @@ class Solution {
                 l=mid+1;
             }
         }
-        res.add(p);
+        res.add(first);
         
-        l=0; h=arr.length-1; p=-1;
+        l=0; h=arr.length-1;
         while (l<=h) 
         {
             mid=l+(h-l)/2;
             if (arr[mid]==x) 
             {
-                p=mid;
+                last=mid;
                 l=mid+1; 
             } 
-            else if (arr[mid]>x) 
+            else if(arr[mid]>x) 
             {
                 h=mid-1;
             } 
@@ -39,7 +40,7 @@ class Solution {
                 l=mid+1;
             }
         }
-        res.add(p);
+        res.add(last);
         return res;
     }
 }
