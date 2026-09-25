f47b8ca4e55026db93aa6bb2c16421fa480e0a89
Krishs15 <krishmill19@gmail.com>
2026-09-25 09:55:43 +0530

    ✅ Minimum Swaps to Sort | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- gfg/medium/minimum-swaps/solution.java
+++ gfg/medium/minimum-swaps/solution.java
@@ -5,11 +5,12 @@ class Solution {
         int i,count=0;
         int temp[]=arr.clone();
         Arrays.sort(temp);
+        
         HashMap<Integer,Integer> map=new HashMap<>();
+        
         for(i=0;i<n;i++)
-        {
             map.put(arr[i],i);
-        }
+            
         for(i=0;i<n;i++)
         {
             if(arr[i]!=temp[i])
